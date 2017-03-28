98cd4d3ea5ec768d964ffc5d6258b7bb96972493
greatstar <greatstar@greatstardeiMac.local>
2017-03-28 15:28:02 +0800

    Initial Commit

---
+++ SyncSchedule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SyncSchedule
+//
+//  Created by greatstar on 2017/3/28.
+//  Copyright © 2017年 greatstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
